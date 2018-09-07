b87bf1e9bafc085233ab69ef62971f7079d834c3
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-09-07 17:51:26 -0400

    Displaying Score and Text (a little issue)
    I can implement the count text just fine
    The video is using .text in order to use a ToString but it would give me an error message if i tried to use it like that saying something like "the pointer is not point to an instance of the class"

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerControl : MonoBehaviour
 {
-    private Rigidbody rb;
     public float speed;
+    public Text countText;
+    //public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        //winText.text = "";
     }
     void FixedUpdate()
     {
@@ -26,6 +36,17 @@ public class PlayerControl : MonoBehaviour
         if(other.gameObject.CompareTag("pick up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count;
+       /* if(count >= 12)
+        {
+            winText.text = "You Win";
+        }*/
     }
 }
